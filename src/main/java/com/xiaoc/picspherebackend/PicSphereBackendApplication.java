ff0db990393b33b977d484927d7e49b0f8af1154
XiaoC <2763645165@qq.com>
2025-02-05 10:17:21 +0800

    初始化

---
+++ src/main/java/com/xiaoc/picspherebackend/PicSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoc.picspherebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicSphereBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicSphereBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoc/picspherebackend/PicSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoc.picspherebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicSphereBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
